f1144334a9829a0db39be4a5be0c3d16959616b8
shinpei-nonaka <shinpei_nonaka@jrass.jp>
2022-09-15 14:36:03 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by PC-SYSKAI556 on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
